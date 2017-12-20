41991e31682aca2ab9975949e88e771a940bd5d8
Samaune Yim <sam.aune@hotmail.com>
2017-12-20 23:02:15 +0700

    update ui

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../shared/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,8 +9,49 @@ import { UserService } from '../../shared/user.service';
 })
 export class NavbarComponent implements OnInit {
   public isCollapsed = true;
+  public itemsNonFocusable: any;
+  constructor(public userService: UserService, public route: Router) { 
+   this.itemsNonFocusable = [
+    {
+      key: 'dashboard',
+      name: 'Dashboard',
+      icon: 'ViewDashboard',
+      onClick: () => { this.routeLink('/home') }
+    }, {
+      key: 'forms', 
+      name: 'Forms',
+      icon: 'OfficeFormsLogo',
+      onClick: () => { this.routeLink('/event') }
+    }, {
+      key: 'Report', 
+      name: 'Reports',
+      icon: 'CRMReport',
+      onClick: () => { this.routeLink('/event') }
+    }, {
+      key: 'settings',
+      name: 'Settings',
+      icon: 'Settings',
+      ariaLabel: 'New. Use left and right arrow keys to navigate',
+      onClick: () => { this.routeLink('/home') },
+      items: [
+        {
+          key: 'emailMessage',
+          name: 'Email message',
+          icon: 'Mail'
+        },
+        {
+          key: 'calendarEvent',
+          name: 'Calendar event',
+          icon: 'Calendar'
+        }
+      ]
+    }
+  ];
+  }
 
-  constructor(public userService: UserService) { }
+  routeLink(url: string){
+    this.route.navigateByUrl(url);
+  }
 
   ngOnInit() {
   }
@@ -19,3 +61,47 @@ export class NavbarComponent implements OnInit {
   }
 
 }
+
+export const itemsNonFocusable = [
+  {
+    key: 'newItem',
+    name: 'New',
+    icon: 'Add',
+    ariaLabel: 'New. Use left and right arrow keys to navigate',
+    onClick: () => { return; },
+    items: [
+      {
+        key: 'emailMessage',
+        name: 'Email message',
+        icon: 'Mail'
+      },
+      {
+        key: 'calendarEvent',
+        name: 'Calendar event',
+        icon: 'Calendar'
+      }
+    ]
+  },
+  {
+    key: 'upload',
+    name: 'Upload',
+    icon: 'Upload',
+    onClick: () => { return; },
+    ['data-automation-id']: 'uploadNonFocusButton'
+  }
+];
+
+export const farItemsNonFocusable = [
+  {
+    key: 'saveStatus',
+    name: 'Your page has been saved',
+    icon: 'CheckMark',
+    ['data-automation-id']: 'saveStatusCheckMark'
+  },
+  {
+    key: 'publish',
+    name: 'Publish',
+    icon: 'ReadingMode',
+    onClick: () => { return; }
+  }
+];
